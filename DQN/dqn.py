01c3a8581f43a7607930c6bbec06e46381001ebc
Junwen <junwen@MBP-ZHONG.local>
2021-01-20 22:16:47 -0800

    Implemented dqn. Next, training process.

--- DQN/dqn.py
+++ DQN/dqn.py
@@ -1,5 +1,18 @@
 from GridWorld import Gridworld
-import torch.nn as nn
 import torch
 import numpy as np
 
+model = torch.nn.Sequential(
+    torch.nn.Linear(64, 150),
+    torch.nn.ReLU(),
+    torch.nn.Linear(150, 100),
+    torch.nn.ReLU(),
+    torch.nn.Linear(100, 4)
+)
+# goal is to learn the optimal Q-function (defined in Bellman equation)
+loss_fn = torch.nn.MSELoss()
+lr = 1e-3
+optimizer = torch.optim.Adam(model.parameters(), lr=lr)
+
+gamma = 0.9
+alpha = 0.2
